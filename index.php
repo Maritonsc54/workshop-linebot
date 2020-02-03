5c0903de510edf32f59c9469c333fa6cebd0884f
kanawoot <mariton_sc54@hotmail.com>
2020-02-03 18:48:11 +0700

    flax

--- index.php
+++ index.php
@@ -138,7 +138,16 @@
                           ]
                         ]
                       ];
-                    $bot->replyMessageNew($bot->replyToken,  $jsonFlex );
+
+                    // $data = [
+                    //     'replyToken' => $reply_token,
+                    //     'messages' => [$jsonFlex]
+                    // ];
+            
+                    // print_r($data);
+            
+                    $post_body = json_encode($jsonFlex, JSON_UNESCAPED_UNICODE);
+                    $bot->replyMessageNew($bot->replyToken,  $post_body );
                     break;
                 case "campaign:inactive":
                     $bot->replyMessageNew($bot->replyToken, 'inactive===>'.$getMessage);
@@ -157,5 +166,20 @@
         echo $bot->response->getHTTPStatus . ' ' . $bot->response->getRawBody(); exit();
 
     }
+
+
+function send_reply_message($url, $post_header, $post_body)
+{
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $post_header);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $post_body);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    $result = curl_exec($ch);
+    curl_close($ch);
+
+    return $result;
+}
 
 ?>
