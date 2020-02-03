f93b628483719de180df05b20cacb34a38f538d6
kanawoot <mariton_sc54@hotmail.com>
2020-02-03 12:06:12 +0700

    index 12

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 
 	// require('PushmessageClass.php');
-	// require('SendmessageClass.php') ;
+	require('SendmessageClass.php') ;
 
 	// $tempush = new PushmessageClass();
-	// $sendmsg = new SendmessageClass();
+	$sendmsg = new SendmessageClass();
 	
     //$accessToken = "eyJhbGciOiJIUzI1NiJ9.UFWcrQH1jMyi3gPzq-WsOclipRRJqbctbksu5wjz9cXyCWMRb_wMEyyA9t3NA84sgQ27e0HRLErYLi-RFgireLnDpxvnGs7LZspA7PoKvR8U8oavJwefuO35V4utaE4VSBZVo27Lr3dMeNrmsHt0oXKaDLZrpinCCl0vGq_zOok.lcKWyUj-K7i-ANwbTfEnVl8889TB-bIhrAarge5hm5o";
     $accessToken = "3ckym0tOjI/vnueETubwqk2XibSK3Ydt63cEFos2UZqmEBzLl0zWcAsXOc7SzpdYUubGw+lNTxn2Q3ywndT4G94EtevAfLNj8TMTJA/az5KJmKu/GIz/y1xgaJe7+TcQaf0fMBgAa0poVIqXs40K0gdB04t89/1O/w1cDnyilFU=";
@@ -29,7 +29,7 @@
 	
 	//get message reply
     $message = strtolower($arrayJson['events'][0]['message']['text']);
-print_r($message);
+echo "55555";
 exit;
 	#ตัวอย่าง Message Type "Text"
     if($message == "campaign"){
@@ -50,9 +50,9 @@ exit;
         $arrayPostData['messages'][0]['text'] = $arrayJson['events'][0]['replyToken'];
         $sendmsg->replyMsg($arrayHeader,$arrayPostData);
     }else if($message == "gpc"){
-		$sendmsg->pushMsg($arrayHeader,$tempush->campaign_use($id , $message));
+		// $sendmsg->pushMsg($arrayHeader,$tempush->campaign_use($id , $message));
     }else if($message == "1"){
-		$sendmsg->pushMsg($arrayHeader,$tempush->flex_button($id));
+		// $sendmsg->pushMsg($arrayHeader,$tempush->flex_button($id));
     }
 	
 	
