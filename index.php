236fe99b50fd9763e797b2b6920bc250deeab685
kanawoot <mariton_sc54@hotmail.com>
2020-02-03 18:10:46 +0700

    index 24

--- index.php
+++ index.php
@@ -4,9 +4,14 @@
     // Line Setting
     $channelSecret = '067ef242eec79c066cdef6793f2f0488';
     $access_token  = '3ckym0tOjI/vnueETubwqk2XibSK3Ydt63cEFos2UZqmEBzLl0zWcAsXOc7SzpdYUubGw+lNTxn2Q3ywndT4G94EtevAfLNj8TMTJA/az5KJmKu/GIz/y1xgaJe7+TcQaf0fMBgAa0poVIqXs40K0gdB04t89/1O/w1cDnyilFU=';
+    $richMenuId = "richmenu-82e6ec42a68a15ef62f812a2bf98fd90";
+    $richMenuImagePath = "richmenu-template-03.png";
 
     $bot = new LineBotLibrary($channelSecret, $access_token);
     if (!empty($bot->isEvents)) {
+        //Upload rich menu image
+        $bot->uploadRichMenuImage($richMenuId, $richMenuImagePath, 'image/jpeg');
+
         $getId = $bot->message->id;
         $getMessage = strtolower($bot->message->text);
         $getType =  $bot->message->type;
